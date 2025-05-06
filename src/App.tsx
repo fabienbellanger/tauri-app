4c88f59ebd91b6f265784642b918ef1f44cd07f9
Fabien BELLANGER <valentil@gmail.com>
2025-05-06 23:55:22 +0200

    Install Tailwind CSS and DaisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
+import {useRef, useState} from "react";
+import {invoke} from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+    const [greetMsg, setGreetMsg] = useState("");
+    const [name, setName] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+    async function greet() {
+        if (name !== "") {
+            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+            setGreetMsg(await invoke("greet", {name}));
+            setName("");
+        }
+        
+        inputRef.current?.focus();
+    }
 
-  return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+    return (
+        <main className="flex flex-col justify-center items-center h-screen">
+            <h1 className="mb-8 text-4xl">Welcome to Tauri + React</h1>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more...</p>
-
-      <form
-        className="row"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+            <form className="flex flew-row gap-4"
+                  onSubmit={async (e) => {
+                      e.preventDefault();
+                      await greet();
+                  }}
+            >
+                <input className="input input-primary"
+                       ref={inputRef}
+                       value={name}
+                       onChange={(e) => setName(e.currentTarget.value)}
+                       placeholder="Enter a name..."
+                />
+                <button className="btn btn-primary" type="submit">Greet</button>
+            </form>
+            <p className="mt-8">{greetMsg}</p>
+        </main>
+    );
 }
 
 export default App;

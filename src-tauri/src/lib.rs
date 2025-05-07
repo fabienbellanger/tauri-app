8931a4c0667375281bf94f4dd53d1271b080fde5
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-05-07 16:30:33 +0200

    WIP

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -7,6 +10,11 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            app.get_webview_window("main").unwrap().open_devtools();
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import {useRef, useState} from "react";
-import {invoke} from "@tauri-apps/api/core";
-import "./App.css";
+import { useRef, useState } from 'react';
+import { invoke } from '@tauri-apps/api/core';
+import './App.css';
+import ThemeControler from './components/ThemeControler';
 
 function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [name, setName] = useState("");
+    const [greetMsg, setGreetMsg] = useState('');
+    const [name, setName] = useState('');
     const inputRef = useRef<HTMLInputElement>(null);
 
     async function greet() {
-        if (name !== "") {
-            // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-            setGreetMsg(await invoke("greet", {name}));
-            setName("");
+        if (name !== '') {
+            setGreetMsg(await invoke('greet', { name }));
+            setName('');
         }
-        
+
         inputRef.current?.focus();
     }
 
@@ -21,21 +21,26 @@ function App() {
         <main className="flex flex-col justify-center items-center h-screen">
             <h1 className="mb-8 text-4xl">Welcome to Tauri + React</h1>
 
-            <form className="flex flew-row gap-4"
-                  onSubmit={async (e) => {
-                      e.preventDefault();
-                      await greet();
-                  }}
+            <form
+                className="flex flew-row justify-center gap-4 w-screen"
+                onSubmit={async (e) => {
+                    e.preventDefault();
+                    await greet();
+                }}
             >
-                <input className="input input-primary"
-                       ref={inputRef}
-                       value={name}
-                       onChange={(e) => setName(e.currentTarget.value)}
-                       placeholder="Enter a name..."
+                <input
+                    className="input input-primary"
+                    ref={inputRef}
+                    value={name}
+                    onChange={(e) => setName(e.currentTarget.value)}
+                    placeholder="Enter a name..."
                 />
-                <button className="btn btn-primary" type="submit">Greet</button>
+                <button className="btn btn-primary" type="submit">
+                    Greet
+                </button>
             </form>
             <p className="mt-8">{greetMsg}</p>
+            <ThemeControler />
         </main>
     );
 }
